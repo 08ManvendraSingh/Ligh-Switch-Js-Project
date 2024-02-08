09d739cf337a24074b8dfe9f152fe62a26ca593e
Manvendra Singh <08singhmanvendra@gmail.com>
2024-02-08 17:04:53 +0530

    initial commit

---
+++ lightswitch.js
@@ -0,0 +1,10 @@
+var a=document.getElementById('one');
+var b=document.getElementById('two');
+function two(){
+	b.style.display='none';
+	a.style.display='block';
+}
+function one(){
+	a.style.display='none';
+	b.style.display='block';
+}
